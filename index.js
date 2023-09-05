0acc4306ca41ad8d5026cec5d10106e675703544
Ryan Wilson <ryanjoelwilson@gmail.com>
2023-09-05 20:05:01 +0100

    Prisma schema added

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const app = express();
+
+app.use(express.json());
---
+++ prisma/migrations/20230905182853_initial/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" STRING NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
